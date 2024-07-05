18e2c215a816caf3f3cffc1223347fbc8802290e
Omole Oluwadamilare Daniel <domole235@gmail.com>
2024-07-05 14:45:11 +0100

    Refracturing codes

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
 import express from "express";
-import dotenv from "dotenv";
-import geoIp2 from "geoip-lite2";
 import axios from "axios";
-
-dotenv.config();
+import requestIp from "request-ip";
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const port = 3000;
 
-app.listen(PORT, () => {
-  console.log("Listening on Port: ", PORT);
-});
+app.use(requestIp.mw());
 
 app.get("/api/hello", async (req, res) => {
-  try {
-    //Setting query to be Guest if the user did not add any query
-    const visitorName = req.query.visitor_name || "Guest";
-    const visitorIpFull =
-      req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  let clientIp = req.clientIp;
+  const visitorName = req.query.visitor_name || "Visitor";
 
-    // Handle IPv6 addresses
-    const visitorIp = visitorIpFull.includes(":")
-      ? visitorIpFull.split(":").pop()
-      : visitorIpFull;
-    //Fetch city using geoip-lite2
-    const visitorLocation = geoIp2.lookup(visitorIp);
-    if (!visitorLocation) {
-      throw new Error("Unable to determine location from IP address");
-    }
-    //Handling error incase location is not fetched
-    const city = visitorLocation.city || "Unknown City";
+  if (clientIp === "::1") {
+    clientIp = "8.8.8.8"; // Use a sample IP for testing, or handle differently
+  }
+
+  try {
+    const locationResponse = await axios.get(
+      `http://ip-api.com/json/${clientIp}`
+    );
+    const locationData = locationResponse.data;
 
     const weatherResponse = await axios.get(
-      `https://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=${city}`
+      `http://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}=${locationData.city}`
     );
+    const weatherData = weatherResponse.data;
 
-    const temperature = weatherResponse.data.current.temp_c;
-
-    res.status(200).json({
-      client_ip: visitorIp,
-      location: city,
-      greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${city}.`,
+    res.json({
+      client_ip: clientIp,
+      location: locationData.city,
+      greeting: `Hello, ${visitorName}! The temperature is ${weatherData.current.temp_c} degrees Celsius in ${locationData.city}`,
     });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to fetch location or weather information" });
   }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
