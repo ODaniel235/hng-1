d53e211be0e5c0fdda855b42f7dd90d9594381b5
Omole Oluwadamilare Daniel <domole235@gmail.com>
2024-07-05 15:27:45 +0100

    Refracturing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import express from "express";
 import axios from "axios";
+import dotenv from "dotenv";
+import express from "express";
 import requestIp from "request-ip";
-
+dotenv.config();
 const app = express();
 const port = 3000;
 
@@ -16,13 +17,12 @@ app.get("/api/hello", async (req, res) => {
   }
 
   try {
-    const locationResponse = await axios.get(
-      `http://ip-api.com/json/${clientIp}`
-    );
+    const locationResponse = await axios.get(`
+      http://ipapi.co/${clientIp}/json/`);
     const locationData = locationResponse.data;
 
     const weatherResponse = await axios.get(
-      `http://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}=${locationData.city}`
+      ` http://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=${locationData.city}`
     );
     const weatherData = weatherResponse.data;
 
@@ -32,10 +32,8 @@ app.get("/api/hello", async (req, res) => {
       greeting: `Hello, ${visitorName}! The temperature is ${weatherData.current.temp_c} degrees Celsius in ${locationData.city}`,
     });
   } catch (error) {
-    console.error("Error fetching data:", error);
-    res
-      .status(500)
-      .json({ error: "Failed to fetch location or weather information" });
+    console.error("Error fetching data:", error.message);
+    res.status(500).json({ error });
   }
 });
 
